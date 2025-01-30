67f9eecdd7d519caaecefcf3181152c63154f8e9
Wil Cram <wcram@wisc.edu>
2025-01-30 11:45:35 -0600

    Setup models and scraping

--- charts/models.py
+++ charts/models.py
@@ -1,3 +1,34 @@
+from datetime import timedelta
+from django.utils import timezone
 from django.db import models
 
-# Create your models here.
+
+class MetricType(models.Model):
+    display_name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.display_name
+
+    def get_recent_data(self):
+        cutoff = timezone.now() - timedelta(days=30)
+        return self.datapoints.filter(timestamp__gte=cutoff).order_by("timestamp")
+
+
+class TimeSeriesMetric(models.Model):
+    metric_type = models.ForeignKey(
+        MetricType, on_delete=models.CASCADE, related_name="datapoints")
+    timestamp = models.DateTimeField(db_index=True)
+    value = models.IntegerField
+
+    class Meta:
+        indexes = [
+            models.Index(fields=['timestamp']),
+            models.Index(fields=['metric_type', 'timestamp']),
+        ]
+        ordering = ['timestamp']
+
+    @classmethod
+    def cleanup_old_data(cls):
+        """Delete data older than 1 month"""
+        cutoff = timezone.now() - timedelta(days=30)
+        cls.objects.filter(timestamp__lt=cutoff).delete()
--- charts/tasks.py
+++ charts/tasks.py
@@ -1,6 +1,8 @@
 from celery import shared_task
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
+from charts.models import MetricType, TimeSeriesMetric
+from django.utils import timezone
 
 
 @shared_task
@@ -9,8 +11,19 @@ def scrape_charts():
     page = urlopen(url)
     html = page.read().decode("utf-8")
     soup = BeautifulSoup(html, 'html.parser')
-    firsts = soup.find_all(class_='tracker-current-count')
-    firsts = map(lambda t: t.text, firsts)
-    seconds = soup.find_all(class_='tracker-max-count')
-    seconds = map(lambda t: t.text, seconds)
-    data = zip(firsts, seconds)
+    titles = map(lambda t: t.text, soup.find_all(class_='tracker-location'))
+    vals = map(lambda t: t.text, soup.find_all(class_='tracker-current-count'))
+
+    if len(titles) != len(vals):
+        print("Titles out of sync with data!")
+        return
+
+    for i in len(titles):
+        name = titles[i]
+        metric_type = MetricType.objects.filter(display_name=name).first()
+        if metric_type is None:
+            metric_type = MetricType.objects.create(display_name=name)
+            metric_type.save()
+        new_metric = TimeSeriesMetric.objects.create(
+            metric_type=metric_type, timestamp=timezone.now(), value=vals[i])
+        new_metric.save()
